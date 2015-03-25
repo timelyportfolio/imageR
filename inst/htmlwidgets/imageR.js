fcd007a9f3cf53b6692f21594e470a74f7b960b7
timelyportfolio <kent.russell@timelyportfolio.com>
2015-03-25 10:13:41 -0500

    add ability to use selector and set display none if img not provided

--- inst/htmlwidgets/imageR.js
+++ inst/htmlwidgets/imageR.js
@@ -11,10 +11,21 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
-
-    el.innerHTML = x.img;
-
-    Intense(el.childNodes)
+    // will consider three cases
+    //  one if img populated then assume we create the element with innerHTML
+    //    and then run Intense on the childNodes
+    if( x.img ){
+      el.style.display = "";
+      el.innerHTML = x.img;
+      Intense(el.childNodes)
+    } else if( x.selector ) {
+      // second case if selector then just run Intense on the selector
+      el.style.display = "none"
+      Intense( document.querySelectorAll( x.selector ) )
+    } else {
+    // third if neither img or selector then assume being used for dependencies
+      el.style.display = "none"
+    }
 
   },
 
